5bc293cc32055b40a0103038d50e834309af5ed7
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-08-28 16:40:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const records = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+];
+
+
+function superbowlWin(record) {
+    let result = record.find(element => element.result ==="W");
+    return (result ? result.year : result=undefined);
+}
